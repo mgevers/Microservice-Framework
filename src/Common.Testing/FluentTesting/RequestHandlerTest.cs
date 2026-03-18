88dbabdbb2737acac85ae395032cb922a2b52aca
Mitchell Gevers <github@mitchell.gevers5.com>
2026-03-18 11:35:17 -0500

    add mediator events to service bus in integration tests

--- src/Common.Testing/FluentTesting/RequestHandlerTest.cs
+++ src/Common.Testing/FluentTesting/RequestHandlerTest.cs
@@ -1,7 +1,9 @@
 ﻿using Ardalis.Result;
 using Common.Testing.Logging;
+using Common.Testing.Mediator;
 using Common.Testing.Persistence;
 using Common.Testing.ServiceBus;
+using MassTransit;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Moq.AutoMock;
@@ -14,27 +16,42 @@ public static class RequestHandlerTest
     public static async Task<RequestHandlerTestResult<TRequestHandler>> Handle<TRequest, TRequestHandler>(
         this HandlerTestSetup<TRequestHandler> testSetup,
         TRequest request)
-        where TRequest : IRequest<Result>
+        where TRequest : class, IRequest<Result>
         where TRequestHandler : class, IRequestHandler<TRequest, Result>
     {
         var messageSession = new TestableMessageSession();
+        var messageContext = new TestableMessageHandlerContext();
+
         var mocker = new AutoMocker();
         testSetup.ConfigureMocker?.Invoke(mocker);
         mocker.Use<IMessageSession>(messageSession);
+        mocker.Use<IMessageHandlerContext>(messageContext);
 
+        using (var fakeMediator = FakeMediator.WithSendResponse())
         using (FakeLoggingDatabase.Initialize(testSetup.LoggingConfiguration))
         using (FakeDatabase.SeedData(testSetup.DatabaseState, testSetup.DatabaseError))
         {
             var handler = mocker.GetRequiredService<TRequestHandler>();
             var result = await handler!.Handle(request, CancellationToken.None);
-            var busState = new ServiceBusState(
-                sentMessages: messageSession.SentMessages.Select(m => m.Message).Cast<IMessage>().ToList(),
-                publishedMessages: messageSession.PublishedMessages.Select(m => m.Message).Cast<IMessage>().ToList(),
-                repliedMessages: Array.Empty<IMessage>());
+            var nServiceBusState = new ServiceBusState(
+                sentMessages: messageSession.SentMessages
+                    .Select(m => m.Message)
+                    .Concat(messageContext.SentMessages.Select(m => m.Message))
+                    .ToList(),
+                publishedMessages: messageSession.PublishedMessages
+                    .Select(m => m.Message)
+                    .Concat(messageContext.PublishedMessages.Select(m => m.Message))
+                    .ToList(),
+                repliedMessages: messageContext.RepliedMessages
+                    .Select(m => m.Message)
+                    .ToList());
+
+            var massTransitBusState = GetMassTransitServiceBusState<TRequest>(mocker);
+            var mediatorBusState = GetMediatorServiceBusState(fakeMediator);
 
             return new RequestHandlerTestResult<TRequestHandler>(
                 FakeDatabase.DatabaseState,
-                busState,
+                CombineServiceBusStates(nServiceBusState, massTransitBusState, mediatorBusState),
                 mocker,
                 result,
                 FakeLoggingDatabase.Logs.ToList());
@@ -44,7 +61,7 @@ public static class RequestHandlerTest
     public static async Task<RequestHandlerTestResult<TRequestHandler, TResult>> Handle<TRequest, TRequestHandler, TResult>(
         this HandlerTestSetup<TRequestHandler, TResult> testSetup,
         TRequest request)
-        where TRequest : IRequest<Result<TResult>>
+        where TRequest : class, IRequest<Result<TResult>>
         where TRequestHandler : class, IRequestHandler<TRequest, Result<TResult>>
     {
         var messageSession = new TestableMessageSession();
@@ -52,22 +69,87 @@ public static class RequestHandlerTest
         testSetup.ConfigureMocker?.Invoke(mocker);
         mocker.Use<IMessageSession>(messageSession);
 
+        using (var fakeMediator = FakeMediator.WithSendResponse())
         using (FakeLoggingDatabase.Initialize(testSetup.LoggingConfiguration))
         using (FakeDatabase.SeedData(testSetup.DatabaseState, testSetup.DatabaseError))
         {
             var handler = mocker.GetRequiredService<TRequestHandler>();
             var result = await handler!.Handle(request, CancellationToken.None);
-            var busState = new ServiceBusState(
+            var nServiceBusState = new ServiceBusState(
                 sentMessages: messageSession.SentMessages.Select(m => m.Message).Cast<IMessage>().ToList(),
                 publishedMessages: messageSession.PublishedMessages.Select(m => m.Message).Cast<IMessage>().ToList(),
                 repliedMessages: Array.Empty<IMessage>());
 
+            var massTransitBusState = GetMassTransitServiceBusState<TRequest>(mocker);
+            var mediatorBusState = GetMediatorServiceBusState(fakeMediator);
+
             return new RequestHandlerTestResult<TRequestHandler, TResult>(
                 FakeDatabase.DatabaseState,
-                busState,
+                CombineServiceBusStates(nServiceBusState, massTransitBusState, mediatorBusState),
                 mocker,
                 result,
                 FakeLoggingDatabase.Logs.ToList());
         }
+    }
+    private static ServiceBusState GetMediatorServiceBusState(FakeMediator fakeMediator)
+    {
+        return new ServiceBusState(
+            sentMessages: fakeMediator.SentMessages,
+            publishedMessages: fakeMediator.PublishedMessages,
+            repliedMessages: Array.Empty<object>());
+    }
+
+    private static ServiceBusState GetMassTransitServiceBusState<TMessage>(AutoMocker autoMocker)
+        where TMessage : class
+    {
+        var mockSendEndpoint = autoMocker.GetMock<ISendEndpoint>();
+        var sendEndpointMessages = mockSendEndpoint.Invocations
+            .Where(i => i.Method.Name.Contains("Send"))
+            .Select(i => i.Arguments.First())
+            .ToList();
+
+        var mockPublishEndpoint = autoMocker.GetMock<IPublishEndpoint>();
+        var publishedEndpointMessages = mockPublishEndpoint.Invocations
+            .Where(i => i.Method.Name.Contains("Publish"))
+            .Select(i => i.Arguments.First())
+            .ToList();
+
+        var mockConsumeContext = autoMocker.GetMock<ConsumeContext<TMessage>>();
+
+        var consumeContextSentMessages = mockConsumeContext.Invocations
+            .Where(i => i.Method.Name.Contains("Send"))
+            .Select(i => i.Arguments.First())
+            .ToList();
+
+        var consumeContextPublishedMessages = mockConsumeContext.Invocations
+            .Where(i => i.Method.Name.Contains("Publish"))
+            .Select(i => i.Arguments.First())
+            .ToList();
+
+        var consumeContextRepliedMessages = mockConsumeContext.Invocations
+            .Where(i => i.Method.Name.Contains("Respond"))
+            .Select(i => i.Arguments.First())
+            .ToList();
+
+        return new ServiceBusState(
+            sentMessages: [.. sendEndpointMessages, .. consumeContextSentMessages],
+            publishedMessages: [.. publishedEndpointMessages, .. consumeContextPublishedMessages],
+            repliedMessages: consumeContextRepliedMessages);
+    }
+
+    private static ServiceBusState CombineServiceBusStates(params ServiceBusState[] states)
+    {
+        List<object> sentMessages = [];
+        List<object> publishedMessages = [];
+        List<object> repliedMessages = [];
+
+        foreach (var state in states)
+        {
+            sentMessages.AddRange(state.SentMessages);
+            publishedMessages.AddRange(state.PublishedMessages);
+            repliedMessages.AddRange(state.RepliedMessages);
+        }
+
+        return new ServiceBusState(sentMessages, publishedMessages, repliedMessages);
     }
 }
---
+++ src/Common.Testing/Mediator/FakeMediator.cs
@@ -0,0 +1,82 @@
+﻿using MediatR;
+using System.Threading.Channels;
+
+namespace Common.Testing.Mediator;
+
+public sealed class FakeMediator : IMediator, IDisposable
+{
+    private readonly AsyncLocal<object?> _sendResponse = new();
+
+    private readonly List<object> _sendChannel = [];
+    private readonly List<object> _publishChannel = [];
+
+    private FakeMediator(object? sendResponse)
+    {
+        _sendResponse.Value = sendResponse;
+    }
+
+    public static FakeMediator WithSendResponse(object? sendResponse = null)
+    {
+        return new FakeMediator(sendResponse);
+    }
+
+    public IReadOnlyList<object> SentMessages => _sendChannel.ToList().AsReadOnly();
+
+    public IReadOnlyList<object> PublishedMessages => _publishChannel.ToList().AsReadOnly();
+
+    public IAsyncEnumerable<TResponse> CreateStream<TResponse>(IStreamRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IAsyncEnumerable<object?> CreateStream(object request, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Publish(object notification, CancellationToken cancellationToken = default)
+    {
+        _publishChannel.Add(notification);
+        return Task.CompletedTask;
+    }
+
+    public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
+        where TNotification : INotification
+    {
+        _publishChannel.Add(notification);
+        return Task.CompletedTask;
+    }
+
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        _sendChannel.Add(request);
+
+        if (_sendResponse.Value is null)
+        {
+            throw new InvalidOperationException("No response was set for this request.");
+        }
+
+        return Task.FromResult((TResponse)_sendResponse.Value);
+    }
+
+    public Task Send<TRequest>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest
+    {
+        _sendChannel.Add(request);
+        return Task.CompletedTask;
+    }
+
+    public Task<object?> Send(object request, CancellationToken cancellationToken = default)
+    {
+        _sendChannel.Add(request);
+        return Task.FromResult(_sendResponse.Value);
+    }
+
+    public void Dispose()
+    {
+        _sendResponse.Value = null;
+
+        _sendChannel.Clear();
+        _publishChannel.Clear();
+    }
+}
