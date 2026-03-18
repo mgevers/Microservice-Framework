eb8993afcd61de105751eb1302d85077c7f6d69a
Mitchell Gevers <github@mitchell.gevers5.com>
2026-03-18 13:21:11 -0500

    add more result extension methods

--- src/Common.LanguageExtensions/Utilities/ResultFunctionalExtensions.cs
+++ src/Common.LanguageExtensions/Utilities/ResultFunctionalExtensions.cs
@@ -158,8 +158,91 @@ public static class ResultFunctionalExtensions
         return Result.CriticalError(errorMessage);
     }
 
+    public static Result<T> Map<T>(this Result result, Func<Result<T>> action)
+    {
+        if (result.IsSuccess)
+        {
+            return action();
+        }
+        else
+        {
+            return result.AsTypedError<T>();
+        }
+    }
+
+    public static async Task<Result<T>> Map<T>(this Task<Result> resultTask, Func<Result<T>> action)
+    {
+        var result = await resultTask;
+
+        if (result.IsSuccess)
+        {
+            return action();
+        }
+        else
+        {
+            return result.AsTypedError<T>();
+        }
+    }
+
+    public static async Task<Result<T>> Map<T>(this Task<Result> resultTask, Func<Task<Result<T>>> action)
+    {
+        var result = await resultTask;
+
+        if (result.IsSuccess)
+        {
+            return await action();
+        }
+        else
+        {
+            return result.AsTypedError<T>();
+        }
+    }
+
+    public static async Task<Result<T>> Map<T>(this Task<Result> resultTask, Func<Task<T>> action)
+    {
+        var result = await resultTask;
+
+        if (result.IsSuccess)
+        {
+            return Result.Success(await action());
+        }
+        else
+        {
+            return result.AsTypedError<T>();
+        }
+    }
+
+    public static async Task<Result<T>> Map<T>(this Task<Result> resultTask, Func<T> action)
+    {
+        var result = await resultTask;
+
+        if (result.IsSuccess)
+        {
+            return Result.Success(action());
+        }
+        else
+        {
+            return result.AsTypedError<T>();
+        }
+    }
+
+
     public static Result<K> Map<T, K>(this Result<T> result, Func<T, Result<K>> action)
     {
+        if (result.IsSuccess)
+        {
+            return action(result.Value);
+        }
+        else
+        {
+            return result.AsTypedError<T, K>();
+        }
+    }
+
+    public static async Task<Result<K>> Map<T, K>(this Task<Result<T>> resultTask, Func<T, K> action)
+    {
+        var result = await resultTask;
+
         if (result.IsSuccess)
         {
             return action(result.Value);
@@ -313,6 +396,16 @@ public static class ResultFunctionalExtensions
         }
 
         return func();
+    }
+
+    public static async Task<Result> Bind(this Result result, Func<Task<Result>> func)
+    {
+        if (!result.IsSuccess)
+        {
+            return result;
+        }
+
+        return await func();
     }
 
     public static async Task<Result> Bind(this Task<Result> resultTask, Func<Task<Result>> func)
